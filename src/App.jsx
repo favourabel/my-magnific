f852b2f6087fd163830f582b596f8b1f9a766bf4
favourabel <nikelwedmaro@gmail.com>
2026-05-21 20:01:46 -0700

    rename pages temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Homepage from "./pages/Homepage";
-import Loginpage from "./pages/Loginpage";
-import Signuppage from "./pages/Signuppage";
+import Homepage from "./pages_temp/Homepage";
+import Loginpage from "./pages_temp/Loginpage";
+import Signuppage from "./pages_temp/Signuppage";
 
 const App = () => {
   return (
